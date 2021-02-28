00a16955ca0a37f56aeefba4552f68adb0c75684
Reid <rlj0713@gmail.com>
2021-02-28 18:59:22 -0500

    Removed if-nil statements

--- lib/cenar/meal.rb
+++ lib/cenar/meal.rb
@@ -14,24 +14,19 @@ class Cenar::Meal
 
     # Returns the meal procedure as a string
     def procedure
-        if @procedure == nil
-            @procedure = get_hash["strInstructions"]
-        end
-        @procedure
+        @procedure = get_hash["strInstructions"]
     end
 
     # Returns the shopping list as an array
     def shopping_list
-        if @shopping_list.empty?
-            ingredient_names = get_hash.select { |k, v| k.include?("strIngredient")}.values
-            ingredient_quantities = get_hash.select { |k, v| k.include?("strMeasure")}.values
-            ingredient_quantities.zip(ingredient_names).each do |quantity, name|
-                if name != nil && name.empty? == false
-                    if quantity != " "
-                        @shopping_list << "#{name}: #{quantity}"
-                    else
-                        @shopping_list << "#{name}"
-                    end
+        ingredient_names = get_hash.select { |k, v| k.include?("strIngredient")}.values
+        ingredient_quantities = get_hash.select { |k, v| k.include?("strMeasure")}.values
+        ingredient_quantities.zip(ingredient_names).each do |quantity, name|
+            if name != nil && name.empty? == false
+                if quantity != " "
+                    @shopping_list << "#{name}: #{quantity}"
+                else
+                    @shopping_list << "#{name}"
                 end
             end
         end
@@ -40,18 +35,12 @@ class Cenar::Meal
 
     # Returns the meal nationality as a string
     def cuisine_nationality
-        if @cuisine_nationality == nil
-            @cuisine_nationality = get_hash["strArea"]
-        end
-        @cuisine_nationality
+        @cuisine_nationality = get_hash["strArea"]
     end
 
     # Returns the YouTube instructional link
     def cuisine_video
-        if @cuisine_video == nil
-            @cuisine_video = get_hash["strYoutube"]
-        end
-        @cuisine_video
+        @cuisine_video = get_hash["strYoutube"]
     end
 
     # Returns the full hash of information for a meal
